310775d670957363051c1a0cf343a330da26ab98
FregosoLeonDavid <80027914+FregosoLeonDavid@users.noreply.github.com>
2022-03-11 16:38:02 -0600

    Add files via upload

---
+++ MainCalc.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+import java.awt.event.*;
+public class MainCalc {
+   public static void main(String args[]) {
+      JFcalc ca = new JFcalc();                                              
+   }
+}
